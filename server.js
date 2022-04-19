b95f53fcf9e11cf9f6c9c00d40861e6f3f3ea58a
ChadH28 <chadhoosain@gmail.com>
2022-04-19 11:50:36 +0200

    make it better

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 const routes = require('./app/routes/index')
 // set port, listen for requests
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 // var corsOptions = {
 //   origin: "http://localhost:3000"
