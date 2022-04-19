0d98aff010225ffe3d2735bcf682f7413f273253
ChadH28 <chadhoosain@gmail.com>
2022-04-19 10:42:51 +0200

    deployment issue fix

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 const app = express();
 const routes = require('./app/routes/index')
 // set port, listen for requests
 const PORT = process.env.PORT || 4000;
 
-var corsOptions = {
-  origin: "http://localhost:3000"
-};
+// var corsOptions = {
+//   origin: "http://localhost:3000"
+// };
+// app.use(cors(corsOptions));
 
-app.use(cors(corsOptions));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+
 // parse requests of content-type - application/json
 app.use(express.json({
   extended: false
 }));
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
+
+// // parse requests of content-type - application/x-www-form-urlencoded
+// app.use(express.urlencoded({ extended: true }));
+
 // bringing in routes
 app.use('/', routes)
 
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
